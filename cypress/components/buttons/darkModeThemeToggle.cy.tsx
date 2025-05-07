c046f3b9e71f09f9f73e2cd7a000d9a7a3652158
Sylvain <sylvain.de.luca@gmail.com>
2025-05-07 12:58:57 +0200

    correction test

--- cypress/components/buttons/darkModeThemeToggle.cy.tsx
+++ cypress/components/buttons/darkModeThemeToggle.cy.tsx
@@ -1,6 +1,6 @@
 
 import DarkModeThemeToggle from '../../../src/Components/Button/DarkModThemeToggle';
-import { ThemeProvider } from '../../../src/contexts/themeContext';
+import { ThemeProvider } from '../../../src/contexts/ThemeContext';
 
 describe('Fonctionnalité de mode sombre', () => {
   beforeEach(() => {
